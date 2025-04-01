3e521dc14094e29276143bb4cecd4af297b6b249
Nadya Polovina <npolovina@berkeley.edu>
2025-03-31 22:00:33 -0400

    update

--- scripts/iam-attach-policies.sh
+++ scripts/iam-attach-policies.sh
@@ -1,4 +1,5 @@
 # Attach necessary policies for ECR and EKS
 aws iam attach-role-policy --role-name github-actions-talk2me-role --policy-arn arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicFullAccess
 aws iam attach-role-policy --role-name github-actions-talk2me-role --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
+aws iam attach-user-policy --user-name github-actions-talk2me-role  --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
 aws iam attach-user-policy --user-name GitHub-Actions-User --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
